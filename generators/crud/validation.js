43c2b1bdbe0f0c249490ede44613052ca3641f23
cRicateau <clementrp@bamlab.fr>
2017-10-09 21:28:00 +0200

    WIP: disable checks

--- generators/crud/validation.js
+++ generators/crud/validation.js
@@ -36,21 +36,21 @@ module.exports = {
         crudProperties.forEach((property, index) => {
           const propertyKeys = Object.keys(property);
     
-          const hasAllCompulsoryKeys = reduce(compulsoryKeys, (truthValue, compulsoryKey) => {
-            const hasCompulsoryKey = includes(propertyKeys, compulsoryKey);
-            if(!hasCompulsoryKey){
-              propertyErrors.push(`Expected ${compulsoryKey} to be part of properties at property of index ${index}`);
-            }
-            return truthValue && hasCompulsoryKey;
-          }, true);
+          // const hasAllCompulsoryKeys = reduce(compulsoryKeys, (truthValue, compulsoryKey) => {
+          //   const hasCompulsoryKey = includes(propertyKeys, compulsoryKey);
+          //   if(!hasCompulsoryKey){
+          //     propertyErrors.push(`Expected ${compulsoryKey} to be part of properties at property of index ${index}`);
+          //   }
+          //   return truthValue && hasCompulsoryKey;
+          // }, true);
     
-          const hasMatchingPropertyTypes = reduce(propertyKeys, (truthValue, propertyKey) => {
-            const isValidType = (typeof property[propertyKey] == propertyCriteria[propertyKey]);
-            if(!isValidType){
-              propertyErrors.push(`Expected ${propertyKey} to be of type ${propertyCriteria[propertyKey]} at property of index ${index}`);
-            }
-            return truthValue && isValidType;
-          }, true);
+          // const hasMatchingPropertyTypes = reduce(propertyKeys, (truthValue, propertyKey) => {
+          //   const isValidType = (typeof property[propertyKey] == propertyCriteria[propertyKey]);
+          //   if(!isValidType){
+          //     propertyErrors.push(`Expected ${propertyKey} to be of type ${propertyCriteria[propertyKey]} at property of index ${index}`);
+          //   }
+          //   return truthValue && isValidType;
+          // }, true);
         })
       })
     })
